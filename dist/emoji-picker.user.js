895fbccaa65e1a5f817bf4c489e107a6419df1da
Burkes <9220052+Burkes@users.noreply.github.com>
2026-07-14 00:25:24 -0300

    Initial Release

---
+++ dist/emoji-picker.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            emoji-picker
+// @description     emoji picker-menu for plug.dj
+// @author          Burkes
+// @include         https://plug.dj/*
+// @exclude         https://plug.dj/dashboard
+// @exclude         https://plug.dj/privacy
+// @exclude         https://plug.dj/terms
+// @exclude         https://plug.dj/ba
+// @exclude         https://plug.dj/plot
+// @exclude         https://plug.dj/about
+// @exclude         https://plug.dj/team
+// @exclude         https://plug.dj/jobs
+// @exclude         https://plug.dj/partners
+// @exclude         https://plug.dj/press
+// @exclude         https://plug.dj/purchase
+// @exclude         https://plug.dj/subscribe
+// @exclude         https://plug.dj/@/*
+// @exclude         https://plug.dj/_/*
+// @version         1.0.0
+// @downloadURL     https://rawgit.com/Burkes/emoji-picker/master/dist/emoji-picker.user.js
+// @updateURL       https://rawgit.com/Burkes/emoji-picker/master/dist/emoji-picker.user.js
+// ==/UserScript==
+(function() {
+    $.getScript("https://rawgit.com/Burkes/emoji-picker/master/dist/emoji-picker.min.js");
+})();
---
+++ src/emoji-picker.user.js
@@ -0,0 +1,3 @@
+(function() {
+    $.getScript("<%= dist_url %>emoji-picker.min.js");
+})();
